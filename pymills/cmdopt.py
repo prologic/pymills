4931a947994b82fbcfe25af23eda0334815ccd25
prologic <none@none>
2005-05-14 13:03:18 +0000

    r28: Removed docs for now
    committer: prologic <prologic>

--- pymills/cmdopt.py
+++ pymills/cmdopt.py
@@ -6,60 +6,18 @@
 # $Author: prologic $
 # $Id: cmdopt.py 1572 2005-04-08 11:16:31Z prologic $
 
-"""Command-line Options module.
-
-This module provides an interface to accessing the command-line
-arguments and options given to a program.
-
-Example:
-usage = "foo -h -a <x> <file>"
-short ="ha-"
-long = ["help", "add"]
-
-cmdopt = CmdOpt(short, long, usage)
-
-opts = cmdopt.getOptions()
-args = cmdopt.getArguments()
-
-if ("-l", "--help") in opts:
-	print "help"
-
-print args[0]
-"""
+"""Command-line Options module."""
 
 import os
 import sys
 import getopt
 
 class Error(Exception):
-	"""Error Class
-
-	Used to raise exceptions upon erors
-	"""
-
 	pass
 
 class CmdOpt:
-	"""cmdopt Class - Interface to program options and arguments.
-
-	This class parses the command-line options and arguments
-	given to a program and returns an interface to these.
-	"""
 
 	def __init__(self, short, long, usage, allowEmpty=True):
-		"""Initialize
-
-		Parse the command-line options and arguments, raise an
-		exception if some oerror occurs. Display the usage if
-		there were no options specified and allowEmpty = False
-
-		Args:
-		   short : string of short options
-		   long : list of strings containing the long options
-		   usage : string of the usage
-		   allowEmpty : bool allow null arguments ?
-		"""
-
 		self._usage = usage
 		self._options = []
 		self._arguments = []
@@ -68,77 +26,28 @@ class CmdOpt:
 			self._options, self._arguments = \
 					getopt.gnu_getopt(sys.argv[1:], short, long)
 		except getopt.GetoptError, e:
-			raise Error("Could not options!")
+			raise Error("Could not read options!")
 
 		if not allowEmpty and self.empty():
 			self.displayUsage()
 			sys.exit(0)
 
 	def empty(self):
-		"""Returns True if no options or arguments
-
-		Args:
-		   None
-
-		Return:
-		   True of False
-		"""
-
 		return self._options == [] and self._arguments == []
 	
 	def displayUsage(self):
-		"""Prints the usage string
-
-		Args:
-		   None
-
-		Return:
-		   None
-		"""
-
 		sys.stdout.write("%s\n" % self._usage)
 		sys.stdout.flush()
 	
 	def getOptions(self):
-		"""Returns the program options
-		
-		Return an instance to an Options class
-		
-		Args:
-		   None
-		
-		Returns:
-		   Object (instance of _Options)
-		"""
-
 		return _Options(self._options)
 
 	def getArguments(self):
-		"""Return the program arguments
-
-		Args:
-		   None
-
-		Returns:
-		   list
-		"""
-
 		return self._arguments
 
 class _Options:
-	"""Class to hold program options
-	
-	Provides a nice interface to the program options
-	This is used internally.
-	"""
 
 	def __init__(self, options):
-		"""Initialize
-
-		Args:
-		   options : list of tuples
-		"""
-
 		self._options = []
 
 		for short, long in options:
