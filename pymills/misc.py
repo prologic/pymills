7544a0d8eb1a377c5f1ecb2cf66c71cc4a329b56
prologic <none@none>
2005-05-14 13:19:03 +0000

    r32: Removed docs for now
    committer: prologic <prologic>

--- pymills/misc.py
+++ pymills/misc.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Filename: misc.py
 # Module:   misc
 # Date:     04th August 2004
@@ -8,39 +6,15 @@
 # $Author: prologic $
 # $Id: Math.py 1572 2005-04-08 11:16:31Z prologic $
 
-"""Math
-
-Math module containg various math routines.
-"""
-
-__version__ = "0.1"
-__copyright__ = "CopyRight (C) 2005 by James Mills"
-__author__ = "James Mills <prologic@shortcircuit.net.au>"
-__url__ = "http://shortcircuit.net.au/~prologic/"
+"""Miscellaneous Module"""
 
 def addPercent(value, percentage):
-	"Add percentage to value returning new value"
-
 	return float(value) * ((float(percentage) / 100.0) + 1.0)
 
 def subPercent(value, percentage):
-	"Subtract percentage to value returning new value"
-
 	return float(value) / ((float(percentage) / 100.0) + 1.0)
 
 def bytes(bytes):
-	"""Return a human-readable form of bytes
-
-	bytes - number of bytes
-
-	Returns:
-	(float, str)
-
-	Where:
-	float - is either a B, KB, MB, GB, TG representation of bytes
-	str   - is the postfix, either one of: B, KB, MB, GB, TG
-	"""
-
 	if bytes >= 1024**4:
 		return (round(bytes / float(1024**4), 2), "TB")
 	elif bytes >= 1024**3:
@@ -53,8 +27,6 @@ def bytes(bytes):
 		return (bytes, "B")
 
 def duration(seconds):
-	"Returns the human-radable duration of seconds"
-
 	days = int(seconds / 60 / 60 / 24)
 	seconds = (seconds) % (60 * 60 * 24)
 	hours = int((seconds / 60 / 60))
@@ -62,6 +34,4 @@ def duration(seconds):
 	mins = int((seconds / 60))
 	seconds = int((seconds) % (60))
 
-	return str(days) + 'd ' + str(hours) + 'h ' + str(mins) + 'm ' + str(seconds) + 's'
-
-#" vim: tabstop=3 nocindent autoindent
+	return "%dd %dh %dm %ds" % (days, hours, mins, secs)
