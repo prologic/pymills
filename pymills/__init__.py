4fe743ad07f005c614a03f4800116c83fc73b84e
prologic <none@none>
2005-11-25 00:57:39 +0000

    r95: * Removed Template and Theme classes * Documented setupPaths function
    committer: prologic <prologic>

--- pymills/__init__.py
+++ pymills/__init__.py
@@ -30,7 +30,7 @@ JamesMills
 
 __name__ = "pymills"
 __desc__ = "James Mills Python Library"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __author__ = "James Mills"
 __email__ = "%s <prologic@shortcircuit.net.au>" % __author__
 __url__ = "http://shortcircuit.net.au/~prologic/"
--- pymills/web.py
+++ pymills/web.py
@@ -11,9 +11,20 @@ applications in python wether you're using plain CGI or
 mod_python
 """
 
-import re
-
 def setupPaths(req, PYTHONPATH=None):
+	"""Return the root paths of the current url given
+	in req. That is: docroot and docpath.
+
+	NOTE: This function only works with mod_python
+
+	
+	req        - mod_python Request object
+	PYTHONPATH - custom path to append to python's searchable
+	             path.
+	
+	Usage:
+	docroot, docpath = setupPaths(req)
+	"""
 
 	import os
 	import sys
@@ -30,38 +41,3 @@ def setupPaths(req, PYTHONPATH=None):
 	docroot = "http://%s%s" % (
 			req.hostname, os.path.dirname(req.uri))
 	return (docroot, docpath)
-
-class Template:
-
-	def __init__(self, docpath="."):
-		self._docpath = docpath
-		self._content = ""
-		self._vars = {}
-	
-	def __str__(self):
-		return self._content % self._vars
-
-	def __setitem__(self, key, value):
-		self._vars[key] = value
-	
-	def load(self, filename):
-		path = self._docpath
-		self._content = open("%s/%s" % (path, filename), 'r').read()
-		# %\((.*?)\)s
-		vars = re.findall("%\((.*?)\)", self._content,
-				re.MULTILINE + 
-				re.DOTALL)
-		for key in vars:
-			self._vars[key] = ""
-	
-class Theme:
-
-	def __init__(self, themepath="."):
-		self._themepath = themepath
-		self._templates = []
-		self._content = ""
-	
-	def load(self, theme):
-		file = "%s/%s/theme.def" % (self._themepath, theme)
-		for i, line in enumerate(open(file, 'r').readlines()):
-			pass
