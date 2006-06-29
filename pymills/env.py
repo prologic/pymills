50e842c709d22815251bb7d41f31413102920b17
prologic <none@none>
2006-06-29 16:06:06 +0000

    r175:  * Closes #25
    committer: prologic <prologic>

--- pymills/env.py
+++ pymills/env.py
@@ -6,24 +6,44 @@
 
 """Environment Container
 
-...
+This is a module to allow you to easily store config files,
+templates, databases, log files and other things related
+to your application easily. It also serves as a general
+container for your application's running data. Instead
+of passing heaps of instances around for various things,
+just pass a single instance of Environment.
 """
 
 import os
 
-try:
-	import default_config
-except:
-	default_config = None
+from config import Configuration
 
 VERSION = 1
 
-class Environment:
+class BaseEnvironment:
+	"""BaseEnvironment(path, name="PyMills",
+	      version=VERSION, defaultConfig=[],
+	      url="http://trac.shortcircuit.net.au/projects/pymills/",
+	      create=False) -> new environment object
+	
+	Creates a new environment object that by default only
+	holds a config file, log file and database.
+
+	This should be sub-classes to provide an application
+	specific environment.
+	"""
 
 	def __init__(self, path, name="PyMills",
+			version=VERSION, defaultConfig=[],
 			url="http://trac.shortcircuit.net.au/projects/pymills/",
 			create=False):
+		"initializes x; see x.__class__.__doc__ for signature"
+
 		self.path = os.path.abspath(os.path.expanduser(path))
+		self.name = name
+		self.version = version
+		self.defaultConfig = defaultConfig
+		self.url = url
 
 		if create:
 			self.create()
@@ -35,6 +55,15 @@ class Environment:
 		self.loadDB()
 
 	def create(self):
+		"""E.create() -> None
+
+		Creates the environment. The environment given
+		by self.path must not already exist.
+
+		Sub-classes may override this to create their own
+		custom environment and may also call this to
+		create the default structure.
+		"""
 
 		def createFile(filename, data=None):
 			fd = open(filename, 'w')
@@ -47,10 +76,6 @@ class Environment:
 		os.mkdir(os.path.join(self.path, "db"))
 		os.mkdir(os.path.join(self.path, "log"))
 		os.mkdir(os.path.join(self.path, "conf"))
-		os.mkdir(os.path.join(self.path, "help"))
-		os.mkdir(os.path.join(self.path, "plugins"))
-		os.mkdir(os.path.join(self.path, "services"))
-		os.mkdir(os.path.join(self.path, "messages"))
 
 		# Create a few files
 		createFile(os.path.join(self.path, "VERSION"),
@@ -62,11 +87,13 @@ class Environment:
 					self.name, self.url))
 
 		# Setup the default configuration
-		createFile(os.path.join(
-			self.path, "conf", "%s.ini") % self.name)
-		if default_config is not None:
-			default_config.createConfig(os.path.join(
-				self.path, "conf", "%s.ini") % self.name)
+		configfile = os.path.join(
+				self.path, "conf", "%s.ini") % self.name
+		createFile(configfile)
+		config = Configuration(configfile)
+		for section, name, value in self.defaultConfig:
+			config.set(section, name, value)
+		config.save()
 		self.loadConfig()
 
 		# Create the database
@@ -79,6 +106,12 @@ class Environment:
 			pass
 
 	def verify(self):
+		"""E.verify() -> None
+
+		Verify that the environment is valid.
+		Sub-classes should not override this.
+		"""
+
 		fd = open(os.path.join(self.path, "VERSION"), "r")
 		try:
 			version = fd.readlines()[0]
@@ -91,20 +124,44 @@ class Environment:
 					"%s Environment Version" % self.name)[1].strip())
 
 	def needsUpgrade(self):
+		"""E.needsUpgrade() -> bool
+
+		Return True if the environment needs upgrading,
+		otherwise return False.
+		Sub-classes should not override this.
+		"""
+
 		return VERSION > self.version
 
 	def loadConfig(self):
-		from config import Configuration
-		self.config = Configuration(
-				os.path.join(
-					self.path, "conf", "%s.ini" % self.name))
-		if default_config is not None:
-			for section, name, value in default_config.CONFIG:
-				self.config.setdefault(section, name, value)
+		"""E.loadConfig() -> None
+
+		Load the configuration file from the environment.
+		Sub-classes should not override this unless they
+		want to load a different type of config file.
+		By default INI-style config files are loaded
+		using Python's Standard ConfigParser Library
+		"""
+
+		configfile = os.path.join(
+				self.path, "conf", "%s.ini") % self.name
+		self.config = Configuration(configfile)
+		for section, name, value in self.defaultConfig:
+			self.config.setdefault(section, name, value)
 	
 	def setupLog(self):
+		"""E.setupLog() -> None
+
+		Setup the log file, logging to the environment.
+		Sub-classes should not override this unless they
+		want to use their own logging mechanism.
+		By default the Python's Standard log module
+		is used.
+		"""
+
 		from log import newLogger
 
+		name = self.name
 		logType = self.config.get("logging", "type")
 		logLevel = self.config.get("logging", "level")
 		logFile = self.config.get("logging", "file")
@@ -112,9 +169,18 @@ class Environment:
 			logFile = os.path.join(self.path, "log", logFile)
 		logID = self.path
 
-		self.log = newLogger(logType, logFile, logLevel, logID)
+		self.log = newLogger(name, logType,
+				logFile, logLevel, logID)
 	
 	def loadDB(self):
+		"""E.loadDB() -> None
+
+		Load the database from the environment.
+		Sub-classes should not override this unless they
+		want to use their own database library.
+		By default the pymills.db library is used.
+		"""
+
 		from pymills.db import Connection
 
 		self.db = Connection(
