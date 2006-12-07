ee8a8691987a079d5e41c8261d69b19b41c79eb1
prologic <none@none>
2006-12-08 03:42:26 +1000

    examples/ircbot:: Added
    committer: James Mills <prologic@shortcircuit.net.au>

---
+++ examples/ircbot.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from pymills.irc import IRC
+from pymills.event import EventManager, filter, listener
+from pymills.sockets import TCPClient
+
+class TestBot(TCPClient, IRC):
+
+	def __init__(self, event):
+		TCPClient.__init__(self, event)
+		IRC.__init__(self)
+
+	@filter()
+	def onDEBUG(self, event):
+		print event
+		return False, event
+
+	@listener("raw")
+	def onRAW(self, data):
+		self.write(data + "\r\n")
+
+	@listener("read")
+	def onREAD(self, line):
+		TCPClient.onREAD(self, line)
+		IRC.onREAD(self, line)
+
+def main():
+
+	event = EventManager()
+	testbot = TestBot(event)
+
+	testbot.open("localhost", 6667)
+
+	testbot.ircUSER(
+			"test",
+			"localhost",
+			"test.shortcircuit.net.au",
+			"Test Bot")
+
+	testbot.ircNICK("test")
+
+	while True:
+		if testbot.connected:
+			testbot.process()
+		event.flush()
+
+if __name__ == "__main__":
+	main()
