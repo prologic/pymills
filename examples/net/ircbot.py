bc8cde1075e3e8cd530c28203c29b3317d6afe25
prologic <none@none>
2008-01-12 00:55:34 +1000

    Updated and fixed to work with current API

--- examples/net/ircbot.py
+++ examples/net/ircbot.py
@@ -2,15 +2,21 @@
 # -*- coding: utf-8 -*-
 # vim: set sw=3 sts=3 ts=3
 
-from pymills.irc import IRC
-from pymills.event import Manager, filter, listener
-from pymills.sockets import TCPClient
+import optparse
+from time import sleep
 
-class TestBot(TCPClient, IRC):
+from pymills.net.irc import IRC
+from pymills.net.sockets import TCPClient
+from pymills import __version__ as systemVersion
+from pymills.event import Manager, filter, listener
+
+USAGE = "%prog [options] <host> [<port>]>"
+VERSION = "%prog v" + systemVersion
+
+class Bot(TCPClient, IRC):
 
 	def __init__(self, event):
-		TCPClient.__init__(self, event)
-		IRC.__init__(self)
+		super(Bot, self).__init__(event)
 
 	@filter()
 	def onDEBUG(self, event):
@@ -21,30 +27,52 @@ class TestBot(TCPClient, IRC):
 	def onRAW(self, data):
 		self.write(data + "\r\n")
 
-	@listener("read")
-	def onREAD(self, line):
-		TCPClient.onREAD(self, line)
-		IRC.onREAD(self, line)
+def parse_options():
+	"""parse_options() -> opts, args
+
+	Parse any command-line options given returning both
+	the parsed options and arguments.
+	"""
+
+	parser = optparse.OptionParser(usage=USAGE, version=VERSION)
+
+	parser.add_option("-s", "--ssl",
+			action="store_true", default=False, dest="ssl",
+			help="Enable Secure Socket Layer (SSL)")
+
+	opts, args = parser.parse_args()
+
+	if len(args) < 1:
+		parser.print_help()
+		raise SystemExit, 1
+
+	return opts, args
 
 def main():
+	opts, args = parse_options()
+
+	host = args[0]
+	if len(args) > 1:
+		try:
+			port = int(args[1])
+		except:
+			port = 6667
+	else:
+		port = 6667
 
 	event = Manager()
-	testbot = TestBot(event)
+	bot = Bot(event)
 
-	testbot.open("localhost", 6667, ssl=True)
+	bot.open(host, port, opts.ssl)
 
-	testbot.ircUSER(
-			"test",
-			"localhost",
-			"test.shortcircuit.net.au",
-			"Test Bot")
-
-	testbot.ircNICK("test")
+	bot.ircUSER("test", "localhost", "localhost", "Test Bot")
+	bot.ircNICK("test")
 
 	while True:
-		if testbot.connected:
-			testbot.process()
+		if bot.connected:
+			bot.process()
 		event.flush()
+		sleep(0.5)
 
 if __name__ == "__main__":
 	main()
