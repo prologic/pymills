9eefdb7432d0fc47ace4f354940c59554b051931
prologic <none@none>
2008-05-27 22:22:58 +1000

    Much simpler BaseEnvironment

--- pymills/env.py
+++ pymills/env.py
@@ -4,74 +4,68 @@
 
 """Environment Container
 
-This is a module to allow you to easily store config files,
-templates, databases, log files and other things related
-to your application easily. It also serves as a general
-container for your application's running data. Instead
-of passing heaps of instances around for various things,
-just pass a single instance of Environment.
+This module contains a base environment that normally should be sub-classed to
+implement an application/system specific environment. An environment is
+designed to act as a container for things like:
+ * config file
+ * logging
+ * database connections
+ * event managers
+ * etc...
+
+Instead of passing multiple objects around a system, you can pass a single
+instance of an environment which holds all the objects needed by a system.
 """
 
 import os
 
-from db import newDB
 from log import newLogger
 from utils import ConfigParser
 
 VERSION = 1
 
+CONFIG = []
+
 class BaseEnvironment(object):
-	"""BaseEnvironment(path, name="PyMills",
-	      version=VERSION, defaultConfig=[], defaultDB=([], []),
-	      url="http://trac.shortcircuit.net.au/projects/pymills/",
-	      create=False) -> new environment object
+	"""BaseEnvironment(path, name="pymills", version=VERSION, config=CONFIG,
+			create=False) -> new environment object
 
 	Creates a new environment object that by default only
-	holds a config file, log file and database.
+	holds configfile and log instances.
 
-	This should be sub-classes to provide an application
+	This should be sub-classed to provide an application
 	specific environment.
 	"""
 
-	def __init__(self, path, name="PyMills",
-			version=VERSION, defaultConfig=[], defaultDB=([], []),
-			url="http://trac.shortcircuit.net.au/projects/pymills/",
-			create=False):
+	def __init__(self, path, name="pymills", version=VERSION, config=CONFIG,
+		create=False):
 		"initializes x; see x.__class__.__doc__ for signature"
 
 		self.path = os.path.abspath(os.path.expanduser(path))
 		self.name = name
 		self.version = version
-		self.defaultConfig = defaultConfig
-		self.defaultDB = defaultDB
-		self.url = url
+		self.config = config
 
 		if create:
 			self.create()
 		else:
 			self.verify()
 
-		os.chdir(self.path)
-
 		self.loadConfig()
 		self.setupLog()
-		self.loadDB()
 
 	def reload(self):
 		"""E.reload() -> None
 
-		Reloads the environment by reloading the config file
-		and database.
+		Reloads the environment.
+		By default this only reloads the config file.
 
 		Sub-classes may override this to create their
-		own custom reload method and may  also call this to
+		own custom reload method and may also call this to
 		reload the default items.
-
-		By default, the config file and database are reloaded.
 		"""
 
 		self.loadConfig()
-		self.loadDB()
 
 	def create(self):
 		"""E.create() -> None
@@ -92,19 +86,15 @@ class BaseEnvironment(object):
 
 		# Create the directory structure
 		os.mkdir(self.path)
-		os.chdir(self.path)
-		os.mkdir(os.path.join(self.path, "db"))
 		os.mkdir(os.path.join(self.path, "log"))
 		os.mkdir(os.path.join(self.path, "conf"))
 
 		# Create a few files
 		createFile(os.path.join(self.path, "VERSION"),
 				"%s Environment Version %d\n" % (
-					self.name, VERSION))
+					self.name, self.version))
 		createFile(os.path.join(self.path, "README"),
-				"This directory contains a %s environment.\n"
-				"Visit %s for more information.\n" % (
-					self.name, self.url))
+				"This directory contains a %s environment." % self.name)
 
 		# Setup the default configuration
 		configfile = os.path.join(
@@ -112,7 +102,7 @@ class BaseEnvironment(object):
 		createFile(configfile)
 		config = ConfigParser()
 		config.read(configfile)
-		for section, name, value in self.defaultConfig:
+		for section, name, value in self.config:
 			if not config.has_section(section):
 				config.add_section(section)
 			config.set(section, name, value)
@@ -120,18 +110,6 @@ class BaseEnvironment(object):
 		config.write(fp)
 		fp.close()
 		self.loadConfig()
-
-		# Create the logger
-		self.setupLog()
-
-		# Create the database
-		self.loadDB()
-		schemas, data = self.defaultDB
-		for schema in schemas:
-			self.db.do(schema)
-		for line in data:
-			self.db.do(line)
-		self.db.commit()
 
 	def verify(self):
 		"""E.verify() -> None
@@ -192,19 +170,8 @@ class BaseEnvironment(object):
 		logLevel = self.config.get("logging", "level")
 		logFile = self.config.get("logging", "file")
 		if not os.path.isabs(logFile):
-			logFile = os.path.join(self.path, "log", logFile)
+			logFile = os.path.join(self.path, logFile)
 		logID = self.path
 
 		self.log = newLogger(name, logType,
 				logFile, logLevel, logID)
-
-	def loadDB(self):
-		"""E.loadDB() -> None
-
-		Load the database from the environment.
-		Sub-classes should not override this unless they
-		want to use their own database library.
-		By default the pymills.db library is used.
-		"""
-
-		self.db = newDB(self.config.get(self.name, "database"))
