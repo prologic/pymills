16a2473b791afc9f966e1ec7fa6128038358404c
prologic <none@none>
2008-07-26 22:10:34 +1000

    Fixed and cleaned up. Added simple display stats at program end

--- examples/event/loops.py
+++ examples/event/loops.py
@@ -2,25 +2,39 @@
 # -*- coding: utf-8 -*-
 # vim: set sw=3 sts=3 ts=3
 
-from pymills.event import listener, Component, Manager, \
-		Event
+import math
+from time import time
 
-class InfiniteLoop(Component):
+from pymills.event import listener, Component, Manager, Event
+
+class Loop(Component):
+
+	events = 0
 
 	@listener("foo")
 	def onFOO(self, event):
-		self.push(Event(), "foo")
+		self.events += 1
+		self.push(Event("Loop"), "foo")
 
 def main():
-
 	e = Manager()
+	loop = Loop(e)
 
-	infiniteLoop = InfiniteLoop(e)
+	e.push(Event("Test"), "foo")
 
-	e.push(Event(), "foo")
+	sTime = time()
 
 	while True:
-		e.flush()
+		try:
+			e.flush()
+		except KeyboardInterrupt:
+			break
+
+	eTime = time()
+	tTime = eTime - sTime
+
+	print "Total Events: %d (%d/s after %0.2fs)" % (
+			loop.events, int(math.ceil(float(loop.events) / tTime)), tTime)
 
 if __name__ == "__main__":
 	main()
