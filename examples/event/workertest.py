0b8f9caa96347626d590d416d187d37a5241255e
prologic <none@none>
2008-07-27 00:17:09 +1000

    Cleaned up worker example

--- examples/event/workertest.py
+++ examples/event/workertest.py
@@ -4,65 +4,63 @@
 
 from time import sleep
 
-from pymills.event import listener, filter, Component, \
-		Worker, Event, Manager
+from pymills.event import listener, filter, workers
+from pymills.event import Component, Worker, Event, Manager
 
-class ThreadOne(Worker):
+class WorkerOne(Worker):
 
 	@listener("hello")
-	def onHELLO(self, event, message=""):
-		self.push(Event("hello from 1 (%s)" % message), "received")
+	def onHELLO(self, message):
+		self.push(
+			Event("Received", "Hello from %s received: %s" % (self, message)),
+			"received")
 
 	def run(self):
 		while self.isRunning():
 			try:
-				self.push(Event(message="foo!"), "hello")
-				sleep(0.001)
+				self.push(Event("Hello", message="Hello from %s" % self), "hello")
+				sleep(1)
 			except KeyboardInterrupt:
 				self.stop()
 
-class ThreadTwo(Worker):
+class WorkerTwo(Worker):
 
 	@listener("hello")
-	def onHELLO(self, event, message=""):
-		self.push(Event("hello from 2 (%s)" % message), "received")
+	def onHELLO(self, message):
+		self.push(
+			Event("Received", "Hello from %s received: %s" % (self, message)),
+			"received")
 
 	def run(self):
 		while self.isRunning():
 			try:
-				self.push(Event(message="bar!"), "hello")
-				sleep(0.001)
+				self.push(Event("Hello", message="Hello from %s" % self), "hello")
+				sleep(1)
 			except KeyboardInterrupt:
 				self.stop()
 
 class Master(Component):
 
-	@filter()
-	def onDEBUG(self, event):
-		print "DEBUG: %s" % event
-
 	@listener("received")
-	def onRECEIVED(self, event, message=""):
+	def onRECEIVED(self, message):
 		print message
 
 def main():
 	e = Manager()
-	one = ThreadOne(e)
-	two = ThreadTwo(e)
-	master = Master(e)
+	Master(e)
+	WorkerOne(e, start=True)
+	WorkerTwo(e, start=True)
 
 	while True:
 		try:
 			e.flush()
-			e.push(Event(message="Hello!"), "hello")
-			sleep(0.001)
+			e.push(Event("Hello", "Hello!"), "hello")
+			sleep(1)
 		except KeyboardInterrupt:
 			break
 
-	one.stop()
-	two.stop()
-
-	e.flush()
+	for worker in workers():
+		worker.stop()
 
 if __name__ == "__main__":
 	main()
