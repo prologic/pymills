4b351e7df5f8002d0fa6e1f8df0db698bd7c9a9c
prologic <none@none>
2008-07-28 01:59:49 +1000

    Fixed and now a working example - half a load balancer... :)

--- examples/net/tcpforward.py
+++ examples/net/tcpforward.py
@@ -2,12 +2,16 @@
 
 import optparse
 
+from pymills.event import *
 from pymills import __version__ as systemVersion
 from pymills.net.sockets import TCPServer, TCPClient
-from pymills.event import listener, Manager, UnhandledEvent, Event
 
-USAGE = "%prog [options]"
+USAGE = "%prog [options] host[:port]"
 VERSION = "%prog v" + systemVersion
+
+###
+### Functions
+###
 
 def parse_options():
 	"""parse_options() -> opts, args
@@ -18,133 +22,137 @@ def parse_options():
 
 	parser = optparse.OptionParser(usage=USAGE, version=VERSION)
 
+	parser.add_option("-b", "--bind",
+			action="store", type="str", default="0.0.0.0", dest="bind",
+			help="Bind to address:[port] (UDP) (test remote events)")
 	parser.add_option("-s", "--ssl",
 			action="store_true", default=False, dest="ssl",
 			help="Enable Secure Socket Layer (SSL)")
-	parser.add_option("-p", "--port",
-			action="store", default=None, dest="port",
-			help="Port to listen on (Required)")
-	parser.add_option("-c", "--connect",
-			action="store", default=None, dest="connect",
-			help="hostname:port to connect to (Required)")
+	parser.add_option("-d", "--debug",
+			action="store_true", default=False, dest="debug",
+			help="Enable debug mode. (Default: False)")
 
 	opts, args = parser.parse_args()
 
-	if opts.port is None:
+	if not args:
 		parser.print_help()
 		raise SystemExit, 1
 	
-	if opts.connect is None:
-		parser.print_help()
-		raise SystemExit, 1
-
 	return opts, args
 
-class NewConnection(Event):
+###
+### Events
+###
 
-	def __init__(self, host, port):
-		super(NewConnection, self).__init__(host, port)
+class ClientOpen(Event): pass
+class ClientData(Event): pass
+class ClientClosed(Event): pass
+class TargetData(Event): pass
+class TargetClosed(Event): pass
 
-class CloseConnection(Event):
-	pass
-
-class Data(Event):
-
-	def __init__(self, data):
-		super(Data, self).__init__(data)
+###
+### Components
+###
 
 class Server(TCPServer):
 
-	__channelPrefix__ = "server"
+	channel = "server"
+	client = None
 
 	@listener("connect")
 	def onCONNECT(self, sock, host, port):
-		print "[Server]: New Connection: %s:%s" % (host, port)
-		self.push(NewConnection(host, port), "client:newconnection")
+		self.client = sock
+		self.push(ClientOpen(), "target:open")
 
 	@listener("disconnect")
 	def onDISCONNECT(self, sock):
-		print "[Server]: Disconnection: %s" % sock
-		self.push(CloseConnection(), "client:closeconnection")
+		self.client = None
+		self.push(ClientClosed(), "target:closed")
 
 	@listener("read")
 	def onREAD(self, sock, data):
-		data = data.strip()
-		print "[Server]: (IN) %s" % data
-		self.push(Data(data), "client:dataout")
+		self.push(ClientData(data), "target:data")
+
+	@listener("closed")
+	def onCLOSED(self):
+		if self.client:
+			self.close(self.client)
+
+	@listener("data")
+	def onDATA(self, data):
+		self.write(self.client, data)
+
+class Target(TCPClient):
+
+	channel = "target"
+
+	connect = None
+
+	@listener("open")
+	def onOPEN(self):
+		self.open(*self.connect)
 	
-	@listener("error")
-	def onERROR(self, sock, error):
-		print "[Server]: ERROR: (%s) %s" % (sock, error)
-	
-	@listener("datain")
-	def onDATAIN(self, data):
-		print "[Server]: (OUT) %s" % data
-		self.broadcast(data)
+	@listener("data")
+	def onDATA(self, data):
+		self.write(data)
 
-class Client(TCPClient):
-
-	__channelPrefix__ = "client"
-
-	def __init__(self, event, host, port):
-		super(Client, self).__init__(event)
-
-		self.host = host
-		self.port = port
-
-	@listener("newconnection")
-	def onNEWCONNECTION(self, host, port):
-		print "[Client]: Connecting to: %s:%s" % (self.host, self.port)
-		self.open(self.host, self.port)
-	
-	@listener("closeconnection")
-	def onCLOSECONNECTION(self):
-		print "[Client]: Closing connection..."
+	@listener("closed")
+	def onCLOSED(self):
 		self.close()
-	
-	@listener("dataout")
-	def onDATAOUT(self, data):
-		print "[Client]: (OUT) %s" % data
-		self.write("%s\n" % data)
-
-	@listener("connect")
-	def onCONNECT(self, host, port):
-		print "[Client]: Connected to %s" % host
-
-	@listener("disconnect")
-	def onDISCONNECT(self):
-		print "[Client]: Disconnected"
-
-	@listener("error")
-	def onERROR(self, error):
-		print "[Client]: ERROR: %s" % error
 
 	@listener("read")
 	def onREAD(self, data):
-		print "[Client]: (IN) %s" % data
-		self.push(Data(data), "server:datain")
+		self.push(TargetData(data), "server:data")
+
+	@listener("disconnect")
+	def onDISCONNECT(self):
+		self.push(TargetClosed(), "server:closed")
+
+###
+### Main
+###
 
 def main():
 	opts, args = parse_options()
 
-	serverPort = int(opts.port)
-	x, y = opts.connect.split(":")
-	clientHost = x
-	clientPort = int(y)
+	if ":" in opts.bind:
+		address, port = opts.bind.split(":")
+		port = int(port)
+		bind = (address, port)
+	else:
+		bind = (opts.bind, 8000)
 
-	e = Manager()
-	server = Server(e, serverPort)
-	client = Client(e, clientHost, clientPort)
+	if ":" in args[0]:
+		address, port = args[0].split(":")
+		port = int(port)
+		connect = (address, port)
+	else:
+		connect = (args[0], 8000)
+
+	debugger.set(opts.debug)
+
+	server = Server(e, address=bind[0], port=bind[1])
+	target = Target(e)
+
+	server.target = target
+	target.connect = connect
 
 	while True:
 		try:
-			server.process()
-			if client.connected:
-				client.process()
 			e.flush()
+			server.poll()
+			e.flush()
+			target.poll()
+		except UnhandledEvent:
+			pass
 		except KeyboardInterrupt:
+			target.close()
+			server.close()
 			break
-	e.flush()
+
+###
+### Entry Point
+###
 
 if __name__ == "__main__":
 	main()
