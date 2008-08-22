937ad95dfe03a4c381d981e8dfae7f111c83a406
prologic <none@none>
2008-08-23 00:35:36 +1000

    Removed old useless deprecated functions. This module will now contain the pymills web server

--- pymills/web.py
+++ pymills/web.py
@@ -3,73 +3,15 @@
 # Date:		27th September 2005
 # Author:	James Mills <prologic@shortcircuit.net.au>
 
-"""Web Library
+"""Web Server Library
 
-This module contains classes to assist in building web-based
-applications in python wether you're using plain CGI or
-mod_python
+This module wraps up various components into a compact
+Web Server Framework. The framework is compatible with
+CherryPy's request object and thus compatible with most
+of CherryPy's tools/filters.
 """
 
-def setupPaths(req, PYTHONPATH=None):
-	"""Return the root paths of the current url given
-	in req. That is: docroot and docpath.
+from pymills.net.http import HTTP
+from pymills.net.sockets import TCPServer
 
-	NOTE: This function only works with mod_python
-
-
-	req        - mod_python Request object
-	PYTHONPATH - custom path to append to python's searchable
-	             path.
-
-	Usage:
-	docroot, docpath = setupPaths(req)
-	"""
-
-	import os
-	import sys
-
-	if PYTHONPATH is None:
-		PYTHONPATH = os.getenv("PYTHONPATH", os.getcwd())
-
-	if PYTHONPATH not in sys.path:
-		sys.path.append(PYTHONPATH)
-
-	docpath = os.path.dirname(req.filename)
-	if docpath not in sys.path:
-		sys.path.append(docpath)
-	docroot = "http://%s%s" % (
-			req.hostname, os.path.dirname(req.uri))
-	return (docroot, docpath)
-
-def escape(s=""):
-	"""escape(s) -> str
-
-	Replace special characters "&", "<" and ">" to HTML-safe
-	sequences
-	"""
-
-	if s == "":
-		return ""
-	return s.replace("&amp;", "&") \
-			.replace("&lt;", "<") \
-			.replace("&gt;", ">") \
-			.replace("&quot;", "\"") \
-			.replace("&#039;", "'") \
-			.replace("&mdash", "--")
-
-def unescape(s=""):
-	"""unescape(s) -> str
-
-	Replace HTML-safe sequences back to their ASCII equivilent
-	characters "&", "<" and ">" ...
-	"""
-
-	if s == "":
-		return ""
-
-	return s.replace("&", "&amp;") \
-			.replace("<", "&lt;") \
-			.replace(">", "&gt;") \
-			.replace("\"", "&quot;") \
-			.replace("'", "&#039;") \
-			.replace("--", "&mdash")
+class Server(TCPServer, HTTP): pass
