c49f62143fae350c09833a5c412ae2e2998b3e22
prologic <none@none>
2008-09-02 21:30:40 +1000

    Added support more compatibility with CehrryPy

--- pymills/headers.py
+++ pymills/headers.py
@@ -5,11 +5,12 @@ so portions are Copyright (C) 2001,2002 Python Software Foundation, and were
 written by Barry Warsaw.
 """
 
+import re
 from types import ListType, TupleType
 
 # Regular expression that matches `special' characters in parameters, the
 # existance of which force quoting of the parameter value.
-import re
+
 tspecials = re.compile(r'[ \(\)<>@,;:\\"/\[\]\?=]')
 
 def _formatparam(param, value=None, quote=1):
@@ -25,6 +26,104 @@ def _formatparam(param, value=None, quote=1):
 			return '%s=%s' % (param, value)
 	else:
 		return param
+
+def header_elements(fieldname, fieldvalue):
+	"""Return a HeaderElement list from a comma-separated header str."""
+	
+	if not fieldvalue:
+		return None
+	headername = fieldname.lower()
+	
+	result = []
+	for element in fieldvalue.split(","):
+		if headername.startswith("accept") or headername == 'te':
+			hv = AcceptElement.from_str(element)
+		else:
+			hv = HeaderElement.from_str(element)
+		result.append(hv)
+	
+	result.sort()
+	return result
+
+class HeaderElement(object):
+	"""An element (with parameters) from an HTTP header's element list."""
+	
+	def __init__(self, value, params=None):
+		self.value = value
+		if params is None:
+			params = {}
+		self.params = params
+	
+	def __unicode__(self):
+		p = [";%s=%s" % (k, v) for k, v in self.params.iteritems()]
+		return u"%s%s" % (self.value, "".join(p))
+	
+	def __str__(self):
+		return str(self.__unicode__())
+	
+	def parse(elementstr):
+		"""Transform 'token;key=val' to ('token', {'key': 'val'})."""
+		# Split the element into a value and parameters. The 'value' may
+		# be of the form, "token=token", but we don't split that here.
+		atoms = [x.strip() for x in elementstr.split(";") if x.strip()]
+		initial_value = atoms.pop(0).strip()
+		params = {}
+		for atom in atoms:
+			atom = [x.strip() for x in atom.split("=", 1) if x.strip()]
+			key = atom.pop(0)
+			if atom:
+				val = atom[0]
+			else:
+				val = ""
+			params[key] = val
+		return initial_value, params
+	parse = staticmethod(parse)
+	
+	def from_str(cls, elementstr):
+		"""Construct an instance from a string of the form 'token;key=val'."""
+		ival, params = cls.parse(elementstr)
+		return cls(ival, params)
+	from_str = classmethod(from_str)
+
+class AcceptElement(HeaderElement):
+	"""An element (with parameters) from an Accept* header's element list.
+	
+	AcceptElement objects are comparable; the more-preferred object will be
+	"less than" the less-preferred object. They are also therefore sortable;
+	if you sort a list of AcceptElement objects, they will be listed in
+	priority order; the most preferred value will be first. Yes, it should
+	have been the other way around, but it's too late to fix now.
+	"""
+	
+	def from_str(cls, elementstr):
+		qvalue = None
+		# The first "q" parameter (if any) separates the initial
+		# media-range parameter(s) (if any) from the accept-params.
+		atoms = q_separator.split(elementstr, 1)
+		media_range = atoms.pop(0).strip()
+		if atoms:
+			# The qvalue for an Accept header can have extensions. The other
+			# headers cannot, but it's easier to parse them as if they did.
+			qvalue = HeaderElement.from_str(atoms[0].strip())
+		
+		media_type, params = cls.parse(media_range)
+		if qvalue is not None:
+			params["q"] = qvalue
+		return cls(media_type, params)
+	from_str = classmethod(from_str)
+	
+	def qvalue(self):
+		val = self.params.get("q", "1")
+		if isinstance(val, HeaderElement):
+			val = val.value
+		return float(val)
+	qvalue = property(qvalue, doc="The qvalue, or priority, of this value.")
+	
+	def __cmp__(self, other):
+		diff = cmp(other.qvalue, self.qvalue)
+		if diff == 0:
+			diff = cmp(str(other), str(self))
+		return diff
 
 class Headers(dict):
 	"""Manage a collection of HTTP response headers"""
@@ -168,3 +267,11 @@ class Headers(dict):
 			else:
 				parts.append(_formatparam(k.replace('_', '-'), v))
 		self._headers.append((_name, "; ".join(parts)))
+
+	def elements(self, key):
+		"""Return a list of HeaderElements for the given header (or None)."""
+		key = str(key).title()
+		h = self.get(key)
+		if h is None:
+			return []
+		return header_elements(key, h)
