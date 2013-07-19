b45d7ad8046c14d16d7aaf17feb6c2d1e194597c
James Mills <j.mills@griffith.edu.au>
2013-07-19 16:30:43 +1000

    Fixed setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,35 +1,6 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup, find_packages
-    HAS_SETUPTOOLS = True
-except ImportError:
-    from distutils.core import setup
-    HAS_SETUPTOOLS = False
-
-if not HAS_SETUPTOOLS:
-    import os
-    from distutils.util import convert_path
-
-    def find_packages(where=".", exclude=()):
-        """Borrowed directly from setuptools"""
-
-        out = []
-        stack = [(convert_path(where), "")]
-        while stack:
-            where, prefix = stack.pop(0)
-            for name in os.listdir(where):
-                fn = os.path.join(where, name)
-                if ("." not in name and os.path.isdir(fn) and 
-                        os.path.isfile(os.path.join(fn, "__init__.py"))):
-                    out.append(prefix+name)
-                    stack.append((fn, prefix + name + "."))
-
-        from fnmatch import fnmatchcase
-        for pat in list(exclude) + ["ez_setup"]:
-            out = [item for item in out if not fnmatchcase(item, pat)]
-
-        return out
+from setuptools import setup, find_packages
 
 from pymills.version import forget_version, get_version, remember_version
 
@@ -40,7 +11,7 @@ setup(
     name="pymills",
     version=get_version(),
     description="Mills Python Library",
-    long_description=open("README", "r").read(),
+    long_description=open("README.rst", "r").read(),
     author="James Mills",
     author_email="James Mills, prologic at shortcircuit dot net dot au",
     url="http://bitbucket.org/prologic/pymills/",
